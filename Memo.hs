f6fe17bbd85a0d585f085e8dd2a386d4fc889174
Jake McArthur <Jake.McArthur@gmail.com>
2012-10-27 16:41:24 +0000

    somewhat direct copy from "Stretching the storage manager: weak pointers and stable names in Haskell"

---
+++ Memo.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE BangPatterns #-}
+module Memo (memo) where
+
+import System.Mem.StableName
+import System.Mem.Weak
+
+import Data.HashTable.IO (BasicHashTable)
+import System.IO.Unsafe (unsafePerformIO)
+
+import qualified Data.HashTable.IO as HashTable
+
+type SNMap a b = BasicHashTable (StableName a) b
+type MemoTable a b = SNMap a (Weak b)
+
+finalizer :: StableName a -> Weak (MemoTable a b) -> IO ()
+finalizer sn weakTbl = do
+  r <- deRefWeak weakTbl
+  case r of
+    Nothing -> return ()
+    Just tbl -> HashTable.delete tbl sn
+
+memo' :: (a -> b) -> MemoTable a b -> Weak (MemoTable a b) -> (a -> b)
+memo' f tbl weakTbl !x = unsafePerformIO $ do
+  sn <- makeStableName x
+  lkp <- HashTable.lookup tbl sn
+  case lkp of
+    Nothing -> notFound sn
+    Just w -> do
+      maybeVal <- deRefWeak w
+      case maybeVal of
+        Nothing -> notFound sn
+        Just val -> return val
+  where notFound sn = do
+          let y = f x
+          weak <- mkWeak x y . Just $ finalizer sn weakTbl
+          HashTable.insert tbl sn weak
+          return y
+
+tableFinalizer :: MemoTable a b -> IO ()
+tableFinalizer = HashTable.mapM_ $ finalize . snd
+
+memo :: (a -> b) -> (a -> b)
+{-# NOINLINE memo #-}
+memo f =
+  let (tbl, weak) = unsafePerformIO $ do
+        tbl <- HashTable.new
+        weak <- mkWeakPtr tbl . Just $ tableFinalizer tbl
+        return (tbl, weak)
+  in memo' f tbl weak
